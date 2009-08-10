efdf62a2a5b23917e85a4339a762823d45ab41f9
Morgan Christiansson <morgan@classicalworld.(none)>
2009-08-10 13:45:52 +0200

    Only regenerate .css files if corresponding .less file has also changed.

--- lib/less_for_rails.rb
+++ lib/less_for_rails.rb
@@ -19,13 +19,15 @@ module LessForRails
   def run(options = {})
     less_sheets = STYLESHEET_PATHS.map {|p| Dir["#{p}/*.less"] }.flatten
     less_sheets.each {|less|
-      engine = Less::Engine.new(File.read(less))
-      css = Less.version > "1.0" ? engine.to_css : engine.to_css(:desc)
-      css = css.delete("\n") if options[:compress]
-      
       destination_file = File.basename(less, File.extname(less))
       destination_directory = "#{Rails.root}/public/stylesheets"
       destination_path = "#{destination_directory}/#{destination_file}.css"
+      
+      next if File.mtime(destination_path) > File.mtime(less)
+
+      engine = Less::Engine.new(File.read(less))
+      css = Less.version > "1.0" ? engine.to_css : engine.to_css(:desc)
+      css = css.delete("\n") if options[:compress]
       
       FileUtils.mkdir_p(destination_directory)
       File.open(destination_path, "w") {|file|
